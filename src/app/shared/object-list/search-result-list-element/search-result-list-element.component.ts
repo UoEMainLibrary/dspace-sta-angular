e989394b0880bdc1f385741f3f5d77f28a21a309
Ben Parkes <benjamin.b.parkes@googlemail.com>
2024-04-22 10:42:58 +0100

    john fix init

--- src/app/shared/object-list/search-result-list-element/search-result-list-element.component.ts
+++ src/app/shared/object-list/search-result-list-element/search-result-list-element.component.ts
@@ -1,18 +1,19 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { SearchResult } from '../../search/models/search-result.model';
+import { BitstreamDataService } from 'src/app/core/data/bitstream-data.service';
+import { APP_CONFIG, AppConfig } from '../../../../config/app-config.interface';
+import { DSONameService } from '../../../core/breadcrumbs/dso-name.service';
 import { DSpaceObject } from '../../../core/shared/dspace-object.model';
+import { Metadata } from '../../../core/shared/metadata.utils';
 import { hasValue } from '../../empty.util';
 import { AbstractListableElementComponent } from '../../object-collection/shared/object-collection-element/abstract-listable-element.component';
+import { SearchResult } from '../../search/models/search-result.model';
 import { TruncatableService } from '../../truncatable/truncatable.service';
-import { Metadata } from '../../../core/shared/metadata.utils';
-import { DSONameService } from '../../../core/breadcrumbs/dso-name.service';
-import { APP_CONFIG, AppConfig } from '../../../../config/app-config.interface';
 
 @Component({
   selector: 'ds-search-result-list-element',
-  template: ``
+  template: ``,
 })
 export class SearchResultListElementComponent<T extends SearchResult<K>, K extends DSpaceObject> extends AbstractListableElementComponent<T> implements OnInit {
   /**
@@ -23,7 +24,8 @@ export class SearchResultListElementComponent<T extends SearchResult<K>, K exten
 
   public constructor(protected truncatableService: TruncatableService,
                      public dsoNameService: DSONameService,
-                     @Inject(APP_CONFIG) protected appConfig?: AppConfig) {
+                     @Inject(APP_CONFIG) protected appConfig?: AppConfig,
+                     bitstreamDataService?: BitstreamDataService ) {
     super(dsoNameService);
   }
 
